7c641325ba15f13fc5e4f45d4a58d0ea784eff86
Hannah Fligel <fligelhannah@gmail.com>
2021-12-27 15:54:22 -0600

    nav bar styling complete

--- src/components/AddRecipeButton/AddRecipeButton.js
+++ src/components/AddRecipeButton/AddRecipeButton.js
@@ -1,8 +1,8 @@
-import React, { useState } from 'react';
-import {useDispatch, useSelector} from 'react-redux';
-import { Link } from 'react-router-dom';
-
-
+import React, { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPlus } from "@fortawesome/free-solid-svg-icons";
 
 // Basic functional component structure for React with default state
 // value setup. When making a new component be sure to replace the
@@ -13,34 +13,32 @@ function AddRecipeButton(props) {
   const store = useSelector((store) => store);
   const dispatch = useDispatch();
 
+  const addIcon = <FontAwesomeIcon icon={faPlus} />;
 
   const [newRecipe, setNewRecipe] = useState({
     recipe_name: "",
   });
 
-  const addRecipe = () =>{
+  const addRecipe = () => {
     dispatch({
-        type:"RESET_RECIPE_PAGE_INFO"
-    })
-    dispatch({
-      type:"RESET_RECIPE_INSTRUCTIONS"
-    })
-    dispatch({
-      type: "RESET_RECIPE_INGREDIENTS"
-    })
-    dispatch({ 
-        type: "ADD_RECIPE", 
-        payload: newRecipe
+      type: "RESET_RECIPE_PAGE_INFO",
     });
-  }
-
-
-
+    dispatch({
+      type: "RESET_RECIPE_INSTRUCTIONS",
+    });
+    dispatch({
+      type: "RESET_RECIPE_INGREDIENTS",
+    });
+    dispatch({
+      type: "ADD_RECIPE",
+      payload: newRecipe,
+    });
+  };
 
   return (
-        <Link onClick={addRecipe} className="navLink" to="/add-recipe">
-            Add
-        </Link>
+    <Link onClick={addRecipe} className="navLink navIcons" to="/add-recipe">
+      {addIcon}
+    </Link>
   );
 }
 
